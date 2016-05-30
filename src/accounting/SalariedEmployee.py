d08cf9ac9bf1b1332dc4e3349e24a18095734e9e
paul <konrad46@hotmail.com>
2016-05-30 19:56:34 -0400

    Initial commit

---
+++ src/accounting/SalariedEmployee.py
@@ -0,0 +1,18 @@
+class SalariedEmployee:
+
+    def __init__(self, employee, first, last):
+        self.__employee_id = employee
+        self.__first_name = first
+        self.__last_name = last
+
+    def set_employee(self, employee):
+        self.__employee_id = employee
+
+    def set_first(self, first):
+        self.__first_name = first
+
+    def set_last(self, last):
+        self.__last_name = last
+
+    def get_full_name(self, last, first):
+        print("Employee: " + last + ", " + first)
